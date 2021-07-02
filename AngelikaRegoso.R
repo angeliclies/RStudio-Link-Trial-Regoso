5215dc06903be7fb0f975adea42f4d21a07af322
angeliclies <angelikaregoso493@gmail.com>
2021-07-02 19:43:56 +0800

    I'll commit a file!

---
+++ AngelikaRegoso.R
@@ -0,0 +1,4 @@
+print("Hi! My name is Ma. Angelika C. Regoso")
+print("I'm 18 years old and I live in Pasig City")
+print("I'm currently studying in Ateneo De Manila University, with the course BS Electronics Engineering")
+print("I'm fond of watching Korean dramas and I also love listening to music")
